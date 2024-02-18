e2b32892c45af960f540c0a827644f7dc163626e
Long Pham <longpham.work23@gmail.com>
2024-02-18 12:00:22 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -43,7 +43,7 @@ const ContentSecurityPolicy = `
     media-src 'none';
     connect-src *;
     font-src 'self' data:;
-    frame-src 'self' *.notblong.io.vn;
+    frame-src 'self' *.codesandbox.io vercel.live *.notblong.io.vn;
 `;
 
 const securityHeaders = [
