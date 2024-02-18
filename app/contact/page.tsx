9a774ba94265fce2b0a2166df0dc1af3f9a25717
Long Pham <longpham.work23@gmail.com>
2024-02-18 11:47:34 +0700

    test

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,9 @@
 export default function Contact() {
     return (
-      <>
-        <iframe
-          src="https://form-app.notblong.io.vn/form/RQOiVVTmBaSehmf"
-          width={700}
-          height={700}
-        ></iframe>
-      </>
+      <iframe
+        src="https://form-app.notblong.io.vn/form/RQOiVVTmBaSehmf"
+        width={700}
+        height={700}
+      ></iframe>
     );
 }
