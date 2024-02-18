ba3edecea3728c1593b2c7a9ddfb3c8bf651a60d
Long Pham <longpham.work23@gmail.com>
2024-02-18 14:45:02 +0700

    support contact form in mobile screen

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,10 @@
 export default function Contact() {
     return (
       <iframe
-        src="https://form-app.notblong.io.vn/form/RQOiVVTmBaSehmf"
+        src="https://form-app.notblong.io.vn/embed/form/RQOiVVTmBaSehmf"
         width={700}
-        height={700}
+        height={450}
+        style={{ width: "100%" }}
       ></iframe>
     );
 }
